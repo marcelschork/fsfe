3ffd0b77cc01c27a0f3bb37becc6e2b0089d3a2a
Marcel Schork <schork.marcel@gmail.com>
2022-12-14 09:19:39 +0100

    Add error 418

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/error', (req, res) => {
+  res.status(418);
+  res.set('X-Custom', 'whoop');
+  res.send('Error')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
